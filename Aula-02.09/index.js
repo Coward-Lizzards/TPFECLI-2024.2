93b5ac8a5f5ba54dfe2e9c46db75fbcbaf56077b
Marina Luisa Lemos Barcelos <Coward_Lizzards@academicos.com>
2024-09-09 14:57:35 -0300

    random num 09/09

--- Aula-02.09/index.js
+++ Aula-02.09/index.js
@@ -1,11 +1,11 @@
 function randomNum(inicio,fim){
-    return Math.random(inicio,fim)
+    return Math.floor(Math.random() * (inicio-fim)) + inicio;
 }
 
-function checkNum(param1,param2){
-    if (param1 < param2){
+function checkNum(userGuess,randomNumero){
+    if (userGuess < randomNumero){
         return "seu número foi menor"
-    } else if (param1 > param2){
+    } else if (userGuess > randomNumero){
         return "seu número foi maior"
     } else{
         return "Venceu"
@@ -13,12 +13,24 @@ function checkNum(param1,param2){
 }
 
 let tentativas = 0
-let userGuess = prompt("insira um numero")
+
 let randomNumero = randomNum(0,1024)
 let ganhou = checkNum(userGuess,randomNumero)
+const numCerto = randomNum(0,1024);
 
 
-while (tentativas < 10 && ganhou === "Venceu"){
-    tentativas ++
-    
+while (tentativasMax > 0) {
+    let userGuess = parseInt(prompt("Adivinhe o número (entre 1 e 100):"));
+    let result = checkGuess(userGuess, numCerto);
+
+    console.log(result);
+
+    if (result === "Parabéns! Você adivinhou o número!") {
+        break;
+    }
+
+    tentativas += 1;
+    if (tentativas === 10) {
+        console.log("Você perdeu! O número era " + correctNumber);
+    }
 }
